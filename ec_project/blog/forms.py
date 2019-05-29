374d7328bd2f57fd46d14480a4effbe831312d54
Brandie Young <brandieyoung98@gmail.com>
2019-05-29 13:59:59 -0400

    first par lines

--- ec_project/blog/forms.py
+++ ec_project/blog/forms.py
@@ -2,3 +2,5 @@ from django import forms
 
 class QueryForm(forms.Form):
     query_text = forms.CharField(widget=forms.Textarea)
+    CHOICES = (('URL', 'URL'), ('Plaintext', 'Plaintext'))
+    text_format = forms.ChoiceField(choices=CHOICES)
--- ec_project/blog/views.py
+++ ec_project/blog/views.py
@@ -5,6 +5,7 @@ import json
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions, ConceptsOptions
 from .languages import ISO639_2
+from urllib.request import urlopen
 
 def landing(request):
 
@@ -16,15 +17,51 @@ def landing(request):
 
         # check whether it's valid:
         if form.is_valid():
-            dirty_text = form.cleaned_data['query_text']
-            service = NaturalLanguageUnderstandingV1(
-            version='2018-03-16',
-            ## url is optional, and defaults to the URL below. Use the correct URL for your region.
-            # url='https://gateway.watsonplatform.net/natural-language-understanding/api',
-            username='7bbd9db3-0045-4031-b3ae-37e74060d817',
-            password='HJk6qs544aYT')
 
-            form= service.analyze(text=dirty_text,features=Features( keywords=KeywordsOptions(limit=3), concepts=ConceptsOptions(limit=3), entities=EntitiesOptions(limit=3))).get_result()
+            dirty_text = form.cleaned_data['query_text']
+            query_format = form.cleaned_data['text_format']
+            service = NaturalLanguageUnderstandingV1(
+                version='2018-03-16',
+                ## url is optional, and defaults to the URL below. Use the correct URL for your region.
+                # url='https://gateway.watsonplatform.net/natural-language-understanding/api',
+                username='7bbd9db3-0045-4031-b3ae-37e74060d817',
+                password='HJk6qs544aYT')
+            if query_format == "Plaintext":
+                form = service.analyze(text=dirty_text, features=Features(keywords=KeywordsOptions(limit=3),
+                                                                          concepts=ConceptsOptions(limit=3),
+                                                                          entities=EntitiesOptions(
+                                                                              limit=3))).get_result()
+            else:
+                form = service.analyze(url=dirty_text, features=Features(keywords=KeywordsOptions(limit=3),
+                                                                         concepts=ConceptsOptions(limit=3),
+                                                                         entities=EntitiesOptions(
+                                                                             limit=3))).get_result()
+                #with urlopen(dirty_text) as dirty_text:
+                 #   dirty_text = dirty_text.decode("UTF-8").readlines()
+            #print(type(dirty_text))
+            #print(dirty_text)
+            par = []
+            first_sent = []
+            str = ""
+            char_counter = 0
+            if query_format == "Plaintext":
+                for c in dirty_text:
+                    char_counter = char_counter+1
+
+                    if c != '\n' and char_counter != len(dirty_text):
+                        str = str + c
+                    else:
+                        par.append(str)
+                        str = ""
+                for p in par:
+                    str2 = ""
+                    for c in p:
+                        if c == '.' or c == '?' or c == '!':
+                            first_sent.append(str2)
+                            break
+                        else:
+                            str2 = str2 + c
+
             lang = form["language"]
             expanded_lang = ISO639_2[lang]
             keywords = form["keywords"]
@@ -43,7 +80,7 @@ def landing(request):
             for e in entities:
                 entities_dict[count] = e["text"]
                 count = count+1
-            final_form= {"language":expanded_lang, "keywords":words, "concepts":concept_links, "entities":entities_dict}
+            final_form= {"language":expanded_lang, "keywords":words, "concepts":concept_links, "entities":entities_dict, "first_sentences":first_sent}
             print(form)
                 # process the data in form.cleaned_data as required
                 # ...

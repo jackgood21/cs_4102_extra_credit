72c7eded23c770089473d6687852eba01da96b46
Jack Good <jg8dp@virginia.edu>
2019-05-31 10:51:37 -0400

    cleaning up the home page

--- ec_project/blog/forms.py
+++ ec_project/blog/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class QueryForm(forms.Form):
 
-    CHOICES = (('URL', 'URL'), ('Plaintext', 'Plaintext'))
+    CHOICES = (('Plaintext', 'Plaintext'), ('URL', 'URL'))
     text_format = forms.ChoiceField(choices=CHOICES, widget=forms.Select(attrs={'style': 'width:120px'}), label = "")
     query_text = forms.CharField(widget=forms.Textarea, label = "")
